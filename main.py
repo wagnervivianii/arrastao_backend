267cc546be14647987cd9ae9e29ed5512d8447c1
Wagner Viviani <devwagnervi@gmail.com>
2025-07-25 13:48:50 -0300

    Usa SECRET_KEY do .env com python-dotenv

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from view.auth_routes import auth_routes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carrega variáveis do .env
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+# Registra o Blueprint de autenticação
+app.register_blueprint(auth_routes)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
